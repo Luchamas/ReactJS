cc5a464e89232db2d82a0a7f8caef62e20b5f083
Luchamas <lucasmabrahao10@gmail.com>
2024-08-19 20:16:07 -0300

    Timer messages

---
+++ timer-message/src/components/Message2.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+function Message2() {
+  const [message2, setMessage2] = useState("Boa Tarde!");
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage2("Boa noite!");
+    }, 10000);
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <div>
+      <h3>{message2}</h3>
+    </div>
+  );
+}
+export default Message2;
