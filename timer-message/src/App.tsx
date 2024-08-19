78d8eda5481ae38cdbc32da242007acc1ca28f82
Luchamas <lucasmabrahao10@gmail.com>
2024-08-19 20:36:59 -0300

    Usando Fetch

--- timer-message/src/App.tsx
+++ timer-message/src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import Timer from "./components/Timer";
 import Message from "./components/Message";
 import Message2 from "./components/Message2";
+import UserList from "./components/UserList";
+import ToggleMessage from "./components/ToggleMessage";
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,8 @@ function App() {
       <Timer />
       <Message />
       <Message2 />
+      <UserList />
+      <ToggleMessage />
     </div>
   );
 }
